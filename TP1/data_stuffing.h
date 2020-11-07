378807ea945b6b0927adcc05977a8c5c33b0c51f
TsarkFC <up201806779@fe.up.pt>
2020-11-07 19:50:05 +0000

    Connection, data_stuffing and file_handler documentation

--- TP1/data_stuffing.h
+++ TP1/data_stuffing.h
@@ -7,5 +7,20 @@ typedef struct {
     int data_size;     /** Number of bytes after stuffing data*/
 } data_stuff;
 
+/**
+ * @brief Applies data stuffing to data received in parameter buffer.
+ * 
+ * @param buffer Data to be stuffed.
+ * @param length Size, in bytes, of data to be stuffed.
+ * @return data_stuff Struct containing stuffed data and its size.
+ */
 data_stuff stuffData(char* buffer,int length);
+
+/**
+ * @brief Reverts data stuffing previously applied.
+ * 
+ * @param buffer Data to be unstuffed.
+ * @param length Size, in bytes, of data to be unstuffed.
+ * @return data_stuff Struct containing original data and its size.
+ */
 data_stuff unstuffData(char * buffer,int length);
